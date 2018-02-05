305c4ba197bfd370b763f6b95050f3e699d2b282
hiro12345 <touroku9002@gmail.com>
2018-02-05 20:13:58 +0900

    test

---
+++ TestApp/TestApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestLibrary1;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = TestClass1.CreateMessage(textBox1.Text);
+            MessageBox.Show(msg);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
---
+++ TestApp/TestLibrary1/TestClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary1
+{
+    public class TestClass1
+    {
+        public static  string CreateMessage(string msg)
+        {
+            return "入力値："+msg;
+        }
+    }
+}
